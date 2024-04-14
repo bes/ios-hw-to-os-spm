5e60e49f2b98491aeff3c0d884d1843426116581
Erik Živković <erik@zivkovic.se>
2024-04-14 10:58:04 +0200

    Fix the package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "IosHwToOs",
+    name: "ios-hw-to-os-spm",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
